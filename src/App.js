3faca134439ebec5b21e81f26c410421aea19302
Jhon Fredy <fredy88x@yahoo.es>
2022-12-21 14:16:48 -0500

    Initial widget project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },{
+        title:'What use React?',
+        content:'React is favorite JS library among engineers'
+    },{
+        title:'How do you use React?',
+        content:'You use React by creating cpmponents'
+    }
+]
+
+export default () => {
+    //return (<div><Accordion items={items}/></div>)
+    return (<div><Search/></div>)
+}
